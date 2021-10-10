dcf63b08813efdd60754c72bcb500ae787af1dce
Arpit <arpitujjwal1@gmail.com>
2021-10-10 21:39:16 +0530

    website

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+  let menu = document.querySelector(".navbar-menu");
+  // if (menu.classList.contains("is-active")) {
+  //   menu.classList.remove("is-active");
+  // } else {
+  //   menu.classList.add("is-active");
+  // }
+  $(".navbar-menu").slideToggle();
+}
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
